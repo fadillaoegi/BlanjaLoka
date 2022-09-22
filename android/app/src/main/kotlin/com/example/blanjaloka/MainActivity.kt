a4ec079fd86de20b0187c927ff45958ba5cd1305
fadillaoegi <fadilfdl2710@gmail.com>
2022-09-22 17:45:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/blanjaloka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blanjaloka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:blanjaloka/pages/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Login(),
+    );
+  }
+}
